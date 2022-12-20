38a9d6edf81689bb043cbb27303e25555348da48
Nate Cornell <nathandcornell@gmail.com>
2022-12-20 11:47:09 -0800

    Add rendering system (doesn't work)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,83 @@
-use ggez::{conf, Context, ContextBuilder, GameResult};
-use ggez::graphics::{self, Color};
-use ggez::event::{self, EventHandler};
-use specs::{World, WorldExt};
+use ggez::{conf, Context, ContextBuilder, event::{self, EventHandler}, GameResult, graphics::{Canvas, Color, DrawParam, Image}};
+use mint::Point2;
+use specs::{join::Join, ReadStorage, System, World, WorldExt};
 use std::path;
 
+const TILE_WIDTH: f32 = 32.0;
+
 mod components;
+
+struct Game {
+    world: World,
+}
+
+impl Game {
+    pub fn new(_context: &mut Context, world: World) -> Game {
+        // Load/create resources like images, etc. here
+
+        Game { world }
+    }
+}
+
+pub struct RenderingSystem<'a> {
+    context: &'a mut Context,
+}
+
+// System Implementation
+impl<'a> System<'a> for RenderingSystem<'a> {
+    // Data
+    type SystemData = (ReadStorage<'a, components::Position>, ReadStorage<'a, components::Renderable>);
+
+    fn run(&mut self, data: Self::SystemData) {
+        let mut canvas = Canvas::from_frame(self.context, Color::WHITE);
+        let (positions, renderables) = data;
+
+        // Clearing the screen (gives the bg color):
+        // graphics::clear(self.context, graphics::Color::new(0.95, 0.95, 0.95, 1.0));
+        // Commented out since it doesn't seem to be in ggez anymore...
+
+        // Get all the renderables with their positions and sort by the position z
+        // This will allow us to have entities layered visually.
+        let mut rendering_data = (&positions, &renderables).join().collect::<Vec<_>>();
+        rendering_data.sort_by_key(|&k| k.0.z);
+
+        // Iterate through all pairs of positions & renderables, load the image
+        // and draw it at the specified position.
+        for (position, renderable) in rendering_data.iter() {
+            // Load the image
+            let imageResult = Image::from_path(self.context, renderable.path.clone());
+
+            if imageResult.is_ok() {
+                let image = imageResult.unwrap();
+                let x = position.x as f32 * TILE_WIDTH;
+                let y = position.y as f32 * TILE_WIDTH;
+
+                // draw
+                let draw_params = DrawParam::new().dest(Point2{x, y});
+                canvas.draw(&image, draw_params);
+            }
+        }
+
+        // Finally, present the context, this will actually display everything
+        // on the screen
+        canvas.finish(self.context);
+    }
+}
+
+impl EventHandler for Game {
+    fn update(&mut self, _context: &mut Context) -> GameResult {
+        // TODO: Update code goes here
+        Ok(())
+    }
+
+    fn draw(&mut self, context: &mut Context) -> GameResult {
+        // TODO: Draw code goes here
+        let mut rs = RenderingSystem {context};
+        rs.run(&self.world);
+
+        return Ok(());
+    }
+}
 
 fn main() {
     let mut world = World::new();
@@ -25,31 +98,4 @@ fn main() {
 
     // Run the game
     event::run(context, event_loop, game);
-}
-
-struct Game {
-    world: World,
-}
-
-impl Game {
-    pub fn new(_context: &mut Context, world: World) -> Game {
-        // Load/create resources like images, etc. here
-
-        Game { world }
-    }
-}
-
-impl EventHandler for Game {
-    fn update(&mut self, _context: &mut Context) -> GameResult {
-        // TODO: Update code goes here
-        Ok(())
-    }
-
-    fn draw(&mut self, context: &mut Context) -> GameResult {
-        let mut canvas = graphics::Canvas::from_frame(context, Color::WHITE);
-        // TODO: Draw code goes here
-        canvas.finish(context);
-
-        return Ok(());
-    }
 }
