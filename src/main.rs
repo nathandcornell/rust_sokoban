ac24c45562dfd662fd26f2ccbf1089d0c40643ec
Nate Cornell <nathandcornell@gmail.com>
2022-12-20 16:03:44 -0800

    Add hidpi scaling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use ggez::{conf, Context, ContextBuilder, event::{self, EventHandler}, GameResult, graphics::{Canvas, Color, DrawParam, Image}};
+use ggez::{conf, Context, ContextBuilder, event::{self, EventHandler},
+    GameResult, graphics::{Canvas, Color, DrawParam, Image},
+    winit::{dpi::{LogicalSize}}
+};
 use mint::Point2;
 use specs::{join::Join, ReadStorage, RunNow, System, World, WorldExt};
 use std::path;
@@ -17,6 +20,7 @@ impl<'a> System<'a> for RenderingSystem<'a> {
     type SystemData = (ReadStorage<'a, components::Position>, ReadStorage<'a, components::Renderable>);
 
     fn run(&mut self, data: Self::SystemData) {
+        let scale_factor: f64 = self.context.gfx.window().scale_factor();
         let mut canvas = Canvas::from_frame(self.context, Color::WHITE);
         let (positions, renderables) = data;
 
@@ -37,11 +41,12 @@ impl<'a> System<'a> for RenderingSystem<'a> {
 
             if image_result.is_ok() {
                 let image = image_result.unwrap();
-                let x = position.x as f32 * TILE_WIDTH;
-                let y = position.y as f32 * TILE_WIDTH;
+                let x = position.x as f32 * TILE_WIDTH * scale_factor as f32;
+                let y = position.y as f32 * TILE_WIDTH * scale_factor as f32;
 
                 // draw
-                let draw_params = DrawParam::new().dest(Point2{x, y});
+                let draw_params = DrawParam::new().dest(Point2{x, y})
+                    .scale(Point2{x: scale_factor as f32, y: scale_factor as f32});
                 canvas.draw(&image, draw_params);
             }
         }
@@ -79,32 +84,62 @@ impl EventHandler for Game {
     }
 }
 
-// Test entities to render:
+// Map loader
+pub fn load_map(world: &mut World, map_string: String) {
+    // Read each line into a Vector:
+    let rows: Vec<&str> = map_string.trim().split('\n').map(|x| x.trim()).collect();
+
+    for (y, row) in rows.iter().enumerate() {
+        let columns: Vec<&str> = row.split(' ').collect();
+
+        for (x, column) in columns.iter().enumerate() {
+            // Calculate the position on the map where this entity will be drawn
+            let position = components::Position {
+                x: x as u8,
+                y: y as u8,
+                z: 0, // we'll override this with the entity z value
+            };
+
+            // Create objects according to our key value:
+            match *column {
+                "." => components::create_floor(world, position),
+                "W" => {
+                    components::create_floor(world, position);
+                    components::create_wall(world, position);
+                }
+                "P" => {
+                    components::create_floor(world, position);
+                    components::create_player(world, position);
+                }
+                "B" => {
+                    components::create_floor(world, position);
+                    components::create_box(world, position);
+                }
+                "S" => {
+                    components::create_floor(world, position);
+                    components::create_box_spot(world, position);
+                }
+                "N" => (),
+                c => panic!("unrecognized map item {}", c),
+            }
+        }
+    }
+}
+
+// Render the game screen:
 pub fn initialize_level(world: &mut World) {
-    components::create_player(
-        world,
-        components::Position {
-            x: 0,
-            y: 0,
-            z: 0,
-        }
-    );
-    components::create_wall(
-        world,
-        components::Position {
-            x: 1,
-            y: 0,
-            z: 0,
-        }
-    );
-    components::create_box(
-        world,
-        components::Position {
-            x: 2,
-            y: 0,
-            z: 0,
-        }
-    );
+    const MAP: &str = "
+    N N W W W W W W
+    W W W . . . . W
+    W . . . B . . W
+    W . . . . . . W
+    W . P . . . . W
+    W . . S . . . W
+    W . . . . . . W
+    W W W W W W W W
+    ";
+
+    load_map(world, MAP.to_string());
 }
 
 fn main() {
@@ -113,9 +148,17 @@ fn main() {
     initialize_level(&mut world);
 
     // Make a Context:
+    // I'm using LogicalSize to help with scaling on different devices:
+    let window_size: Option<LogicalSize<f32>> = Some(LogicalSize::new(800.0, 600.0));
+    let default_window_mode = conf::WindowMode::default();
+    let window_mode = conf::WindowMode {
+        logical_size: window_size,
+        ..default_window_mode
+    };
+
     let (mut context, event_loop) = ContextBuilder::new("rust_sokoban", "Nate Cornell")
         .window_setup(conf::WindowSetup::default().title("Rust Sokoban!"))
-        .window_mode(conf::WindowMode::default().dimensions(800.0,600.0))
+        .window_mode(window_mode)
         .add_resource_path(path::PathBuf::from("./resources"))
         .build()
         .expect("Error! Could not create game context");
